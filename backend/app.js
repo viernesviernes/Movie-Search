07e0e85060d8944b9e6480c8e0717bc0509e9fa6
Jumhar Viernes <jvierne4@my.centennialcollege.ca>
2024-11-28 11:47:31 -0500

    Updated backend with cors

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const axios = require('axios');
 require('dotenv').config({ path: '../.env' });
-
+const cors = require('cors');
+app.use(cors());
 const app = express();
 
 app.listen(3000, console.log("Port 3000 is active."));
